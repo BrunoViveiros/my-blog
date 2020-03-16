7751d5083b3229d4694a6418eddea82812ead771
Ray Zyphs <bruferviveiros@gmail.com>
2020-03-16 19:36:44 -0300

    28. Criando o SocialLinks Component - Parte 1

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/BrunoViveiros",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/BrunoViveiros10",
+  },
+  {
+    label: "Youtube",
+    url: "https://youtube.com/",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/bruno.viv",
+  },
+  {
+    label: "Unsplash",
+    url: "https://unsplash.com",
+  },
+]
+
+export default links
