05e0ab41b9009f2b1008bbcd52c7a4b1fbb28fda
Ray Zyphs <bruferviveiros@gmail.com>
2020-03-17 11:43:03 -0300

    missing values

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // To add the slug field to each post
@@ -36,5 +37,15 @@ exports.createPages = ({ graphql, actions }) => {
         }
       }
     }
-  `)
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve("./src/templates/blog-post.js"),
+        context: {
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
 }
