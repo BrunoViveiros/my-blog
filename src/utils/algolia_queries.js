1ad9189152029d923a367ff778bbe73ca7d3b905
Ray Zyphs <bruferviveiros@gmail.com>
2020-03-21 12:49:16 -0300

    Algolia error AttributeToSnippet sem o S

--- src/utils/algolia_queries.js
+++ src/utils/algolia_queries.js
@@ -40,7 +40,7 @@ const queries = [
     transformer: ({ data }) => flatten(data.posts.edges),
     indexName: `Posts`,
     settings: {
-      attributesToSnippets: ["excerpt:20"],
+      attributesToSnippet: ["excerpt:20"],
     },
   },
 ]
